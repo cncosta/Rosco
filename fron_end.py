d0baa30f17e0908439cc19cf72d3624669cb380d
Carlos Costa <cncosta@uc.cl>
2020-08-07 14:17:13 -0400

    formato

--- fron_end.py
+++ fron_end.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QLabel, QWidget, QApplication, QHBoxLayout
+from PyQt5.QtWidgets import QLabel, QWidget, QApplication
 from PyQt5.QtCore import Qt
 import numpy as np
 import sys
@@ -13,7 +13,6 @@ class PaintLabel(QLabel):
         self.estado = "white"
 
     def mousePressEvent(self, QMouseEvent):
-        print(f"se apreto {self.text()}")
         if self.estado == "white":
             self.setStyleSheet('background-color: green; border-radius: 20px;')
             self.estado = "green"
@@ -23,37 +22,41 @@ class PaintLabel(QLabel):
             self.estado = "red"
         elif self.estado == "red":
             self.estado = "yellow"
-            self.setStyleSheet('background-color: yellow; border-radius: 20px;')
+            self.setStyleSheet('background-color: #f5dd29; border-radius: 20px;')
 
         elif self.estado == "yellow":
             self.estado = "white"
-            self.setStyleSheet('background-color: rgb 0 0 0 0; border-radius: 20px;border: 1px solid blue')
+            self.setStyleSheet('background-color: rgb 0 0 0 0; '
+                               'border-radius: 20px;'
+                               'border: 2px solid blue')
 
 
 class MainWindow(QWidget):
     def __init__(self,*args):
         super().__init__(*args)
         self.letras = {}
-        self.lay_out = QHBoxLayout()
+        self.author = QLabel(self)
+        self.author.setText('Author cncosta')
+        self.author.move(0,0)
+        self.setWindowTitle('Pasapalabra')
+        self.setFixedSize(430,430)
+
+
         for i in pm.letras:
             self.letras.update({i: PaintLabel(i,self)})
         self.set_up()
 
     def set_up(self):
-        for label in self.letras.values():
-
-            label.resize(40, 40)
-            label.setStyleSheet("border: 1px solid blue; "
-                                "border-radius: 20px;")
-
         i = 0
         theta = 2 * np.pi / len(self.letras)
-        for c in self.letras.values():
-
+        for label in self.letras.values():
+            label.resize(40, 40)
+            label.setStyleSheet("border: 2px solid blue; "
+                                "border-radius: 20px;")
             angle = theta * (i)
-            dx = int(round(400 + 165 * np.cos(angle - np.pi/2)))
-            dy = int(round(300 + 165 * np.sin(angle - np.pi/2)))
-            c.move(dx,dy)
+            dx = int(round(200 + 165 * np.cos(angle - np.pi/2)))
+            dy = int(round(200 + 165 * np.sin(angle - np.pi/2)))
+            label.move(dx,dy)
             i += 1
 
 
